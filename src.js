a4e6757ee812ec5d90c80bbbdee515589d4e7863
YoussefNsserALlam <128973505+YoussefNsserALlam@users.noreply.github.com>
2023-04-18 17:52:14 +0200

    Add files via upload

--- src.js
+++ src.js
@@ -3,13 +3,13 @@ xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     let myObj = JSON.parse(this.responseText);
         let chek = document.getElementById("sp");
-        chek.onclick = function(){
+        chek.onclick = function(rr){
             let emal = document.getElementById("exampleInputEmail1").value;
             let pass = document.getElementById("exampleInputPassword1").value;
-            let  error = document.querySelector(".erorr")
+            let  error = document.querySelector(".erorr");
             for(let i = 0; i < myObj.length; i++){
             if(emal === myObj[i].emal && pass === myObj[i].pass){
-                document.write("<h1>Work is underway on this page<h1>")
+                location.href= './home.html'
             }else{
                 error.style.display= "block"
             }
